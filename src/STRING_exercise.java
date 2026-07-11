cea42457da8b2a43dfc97fe64bbf56cbb8982e75
kunal <kunaltilekar79@gmail.com>
2026-07-11 15:56:58 +0530

    init commit

---
+++ src/STRING_exercise.java
@@ -0,0 +1,25 @@
+
+public class STRING_exercise {
+    public static void main(String[] args)
+    {
+     //   int num1 = 12;
+     //   String name = "ROOM";
+
+     //   int length = name.length(); //counts the number of indices in 'name'
+
+      //  char letter = name.charAt(1);  //Tells the character at the specified index
+
+      //  int index = name.indexOf("o");  //tells the first index of the character
+
+      //  int index_last = name.lastIndexOf("o"); //tells the last most appeared index of the character
+
+       // name = name.toLowerCase();  //converts all characters to lower case
+
+       // name = name.toUpperCase();  //converts all characters to upper case
+
+
+      //  System.out.println(name);
+
+
+    }
+}
