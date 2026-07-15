4dbf6aa89248d550fedc422deab929a6252d772f
kunal <kunaltilekar79@gmail.com>
2026-07-15 19:03:58 +0530

    init commit

--- src/Loop.java
+++ src/Loop.java
@@ -1,45 +1,23 @@
 import java.util.Scanner;
 
 public class Loop {
-   public static void main(String[] args) {
+    public static void main(String[] args) {
 
-       Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-       System.out.print("Enter a number greater than 1: ");
-       int n = sc.nextInt();
+        System.out.println("The given array is: 2,5,8,11......n");
 
-       for(int i=n; i>=1; i--){
+        System.out.print("Enter the number of 'n' terms in the AP ");
+        int num1 = sc.nextInt();
 
-           System.out.print(i+ " ");
+        for (int i = 2;i<=num1*3; i+=3) {
 
+            System.out.print(i+" ");
 
-
-
-
-
-           //if(i%3 ==0 && i%2!=0){
-             //  System.out.println(i);
-
-         //  }
-
-           }
-
-
-
-
-
-
-           //System.out.print(i*2 +" "); // easy even numbers
-           // System.out.println(num1); //repeating the same number n number of times.(same can be done for other variable types)
-
-
-
-
-       }
-
+        }
 
 
     }
 
-
+}
 
