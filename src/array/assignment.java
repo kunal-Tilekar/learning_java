440d013824712d17fb0bc6a2e5b66dac8e392cdf
kunal <kunaltilekar79@gmail.com>
2026-07-18 17:28:53 +0530

    init commit

--- src/array/assignment.java
+++ src/array/assignment.java
@@ -19,11 +19,13 @@ public class assignment {
 
         }
 
-        System.out.println("The array is as follows: ");
+        System.out.println("The array with negative elements only is as follows: ");
 
         for(int i=0; i<arr.length;i++){
 
-            System.out.print(-1*arr[i]+" ");
+            if(arr[i]<0) {
+                System.out.print(arr[i] + " ");
+            }
 
         }
 
